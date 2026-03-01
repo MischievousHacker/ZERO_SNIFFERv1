eec9339cb11d947580aab477d4c53ce2b84efc49
MischievousHacker <152664064+MischievousHacker@users.noreply.github.com>
2026-03-01 16:50:12 -0500

    Create attacks.h

---
+++ attacks.h
@@ -0,0 +1,13 @@
+#ifndef ATTACKS_H
+#define ATTACKS_H
+
+void startWifiScan();
+void stopWifiScan();
+
+void startPacketMonitor();
+void stopPacketMonitor();
+
+void startHandshakeCapture();
+void stopHandshakeCapture();
+
+#endif
